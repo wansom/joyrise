52542bd74a77b334d46c3ab72820f3422c8fdd7e
wansom <ochiengwarren10@gmail.com>
2021-11-25 15:25:06 +0300

    revert to previous commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<!-- 
-	This is the main page of the application, the layout component is used here,
-	and the router-view is passed to it.
-	Layout component is dynamically declared based on the layout for each route,
-	specified in routes list router/index.js .
- -->
 
 <template>
 	<div id="app">
--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-/*
-=========================================================
-Muse - Vue Ant Design Dashboard - v1.0.0
-=========================================================
 
-Product Page: https://www.creative-tim.com/product/vue-ant-design-dashboard
-Copyright 2021 Creative Tim (https://www.creative-tim.com)
-Coded by Creative Tim
-
-=========================================================
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
-*/
 
 import Vue from 'vue'
 import Antd from 'ant-design-vue';
